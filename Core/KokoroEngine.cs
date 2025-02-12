494a51585e56fd4481c98a26be657bb502343326
Martin Evans <martindevans@gmail.com>
2025-02-12 18:49:33 +0000

    - Fixed formatting/naming  - Added back in an inner loop for multi step jobs  - Catching cancellation exception within worker thread

--- Core/KokoroEngine.cs
+++ Core/KokoroEngine.cs
@@ -9,25 +9,30 @@ using System.Collections.Concurrent;
 public class KokoroEngine : IDisposable {
     protected readonly KokoroModel model;
     protected readonly BlockingCollection<KokoroJob> queuedJobs = [];
-    private readonly CancellationTokenSource _cancellation = new();
+    private readonly CancellationTokenSource cancellation = new();
 
     /// <summary> Creates a new Kokoro Engine instance, loading the model into memory and initializing a background worker thread to continuously scan for newly queued jobs, dispatching them in order, when it's free. </summary>
     /// <remarks> If 'options' is specified, the model will be loaded with them. This is particularly useful when needing to run on non-CPU backends, as the default backend otherwise is the CPU with 8 threads. </remarks>
-    public KokoroEngine(string modelPath, SessionOptions options = null)
-    {
+    public KokoroEngine(string modelPath, SessionOptions options = null) {
         model = new KokoroModel(modelPath, options);
 
         new Thread(() =>
         {
-            foreach (var kokoroJob in queuedJobs.GetConsumingEnumerable(_cancellation.Token))
-                kokoroJob.Progress(model);
+            try {
+                while (queuedJobs.TryTake(out var kokoroJob, -1, cancellation.Token))
+                    while (!cancellation.IsCancellationRequested && !kokoroJob.isDone)
+                        kokoroJob.Progress(model);
+            }
+            catch (OperationCanceledException) {
+                // If cancellation was requested just swallow the cancellation exception and exit.
+            }
         }).Start();
     }
 
     /// <summary> Enqueues a job for the Kokoro TTS engine, scheduling it to be processed when the engine is free. </summary>
     /// <remarks> The job will be automatically dispatched when all prior jobs have been completed or canceled. Canceled jobs resolve and get skipped when their order arrives. </remarks>
     public KokoroJob EnqueueJob(KokoroJob job) {
-        ObjectDisposedException.ThrowIf(_cancellation.IsCancellationRequested, this);
+        ObjectDisposedException.ThrowIf(cancellation.IsCancellationRequested, this);
         queuedJobs.Add(job);
         return job;
     }
@@ -35,7 +40,7 @@ public class KokoroEngine : IDisposable {
     /// <summary> Immediately cancels any ongoing registered jobs and playbacks, frees memory taken by the model, and notifies the background worker thread to exit. </summary>
     /// <remarks> Note that this will not free up memory that the voices take (~25MB if ALL languages are loaded). </remarks>
     public virtual void Dispose() {
-        _cancellation.Cancel();
+        cancellation.Cancel();
         foreach (var job in queuedJobs) { job.Cancel(); }
         queuedJobs.Dispose();
         model.Dispose();
