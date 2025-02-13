ac5c06e8eec502ccc6748b6b8d4ef497a0b4d535
Lyrcaxis <32474602+Lyrcaxis@users.noreply.github.com>
2025-02-13 23:00:44 +0200

    Apply suggestions from code review
    trim cancellation token and try-catch (+ minor styling)

--- Core/KokoroEngine.cs
+++ Core/KokoroEngine.cs
@@ -9,27 +9,16 @@ using System.Collections.Concurrent;
 public class KokoroEngine : IDisposable {
     protected readonly KokoroModel model;
     protected readonly BlockingCollection<KokoroJob> queuedJobs = [];
-    readonly CancellationTokenSource cancellation = new();
 
     /// <summary> Creates a new Kokoro Engine instance, loading the model into memory and initializing a background worker thread to continuously scan for newly queued jobs, dispatching them in order, when it's free. </summary>
     /// <remarks> If 'options' is specified, the model will be loaded with them. This is particularly useful when needing to run on non-CPU backends, as the default backend otherwise is the CPU with 8 threads. </remarks>
     public KokoroEngine(string modelPath, SessionOptions options = null) {
         model = new KokoroModel(modelPath, options);
 
-        new Thread(() =>
-        {
-            try {
-                while (queuedJobs.TryTake(out var kokoroJob, -1, cancellation.Token))
-                    while (!cancellation.IsCancellationRequested && !kokoroJob.isDone)
-                        kokoroJob.Progress(model);
+        new Thread(() => {
+            foreach (var kokoroJob in queuedJobs.GetConsumingEnumerable()) {
+                while (!kokoroJob.isDone) { kokoroJob.Progress(model); }
             }
-            catch (OperationCanceledException) {
-                // If cancellation was requested just swallow the cancellation exception and exit.
-            }
-
-            foreach (var kokoroJob in queuedJobs.GetConsumingEnumerable())
-                kokoroJob.Cancel();
-            queuedJobs.Dispose();
         }).Start();
     }
 
@@ -43,9 +32,8 @@ public class KokoroEngine : IDisposable {
     /// <summary> Immediately cancels any ongoing registered jobs and playbacks, frees memory taken by the model, and notifies the background worker thread to exit. </summary>
     /// <remarks> Note that this will not free up memory that the voices take (~25MB if ALL languages are loaded). </remarks>
     public virtual void Dispose() {
+        foreach (var job in queuedJobs) { job.Cancel(); }
         queuedJobs.CompleteAdding();
-        cancellation.Cancel();
         model.Dispose();
-        GC.SuppressFinalize(this);
     }
 }
